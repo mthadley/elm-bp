1e7215410edd2b64bf27ad1a5133f02ce6322682
Michael Hadley <mikethadley@gmail.com>
2019-10-09 19:22:05 -0700

    Add direnv and nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+with import (fetchTarball {
+  url =  https://releases.nixos.org/nixpkgs/19.09-darwin/nixpkgs-darwin-19.09pre193404.9bbad4c6254/nixexprs.tar.xz;
+  sha256 = "1hjbn17s33w7zkmd69n26zgjk671rw41frgf6bzfvp1qaq5y74zl";
+}) {};
+
+stdenv.mkDerivation {
+  name = "elm-bp";
+  buildInputs = [
+    gnumake
+    nodejs
+    elmPackages.elm
+    elmPackages.elm-format
+    entr
+  ];
+}

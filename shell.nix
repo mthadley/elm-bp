9ce53e83db8fe0e446ee377f99a2a3ca5f776302
Michael Hadley <mikethadley@gmail.com>
2019-10-09 19:35:15 -0700

    Note about where this url came from

--- shell.nix
+++ shell.nix
@@ -1,4 +1,5 @@
 with import (fetchTarball {
+  # Taken from https://github.com/nixos/nixpkgs-channels
   url =  https://releases.nixos.org/nixpkgs/19.09-darwin/nixpkgs-darwin-19.09pre193404.9bbad4c6254/nixexprs.tar.xz;
   sha256 = "1hjbn17s33w7zkmd69n26zgjk671rw41frgf6bzfvp1qaq5y74zl";
 }) {};
